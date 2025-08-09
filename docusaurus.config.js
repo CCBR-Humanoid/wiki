fbebb13ce1f43464ac898d64d8c3fd3b426c182c
William Forte <willcforte@gmail.com>
2025-08-09 12:54:04 -0400

    remove colors in dark mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -187,7 +187,7 @@ const config = {
       },
       colorMode: {
         defaultMode: 'dark',
-        disableSwitch: false,
+        disableSwitch: true,
         respectPrefersColorScheme: false,
       },
     }),
